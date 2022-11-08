95cb8023fd0e942a58a6de1ae3bc4f53751f0f06
golam saroar <gsaroar@testreach.com>
2022-11-08 13:54:31 -0600

    added factorial

---
+++ factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.WriteLine($"Enter the factorial number");
+        int factNumber = Int32.Parse(Console.ReadLine());
+        int factResult = 1;
+        if (factNumber <=0){
+            Console.WriteLine($"Please enter a positive nubmer");
+            
+        }
+        else
+        {
+            
+        for (int i = 1; i <= factNumber; i++)
+        {
+            factResult = factResult *i;
+        }
+        Console.WriteLine("factorial of {0} is {1}",factNumber,factResult);
+        
+        }
+        
+    }
+}
